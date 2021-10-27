bb5b18c5e5a28a5f70a1b2d62d985ceb6f25ce0d
crazy960 <crazyescape88@gmail.com>
2021-10-27 11:54:20 +0900

    Atomic

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,48 +4,43 @@
 #include <iostream>
 
 #include <thread>
+#include <atomic>
 
-void HelloThread()
+// atomic : All-Or_Nothing
+atomic<int32> sum = 0;
+//int32 sum = 0;
+
+
+void Add()
 {
-    cout << "Hello Ah" << endl;
+    for (int32 i = 0; i < 100'0000; i++)
+    {
+        sum.fetch_add(1);
+        //sum++;
+    }
 }
-void HelloThread_2(int32 num)
+void Sub()
 {
-    cout << num << endl;
+    for (int32 i = 0; i < 100'0000; i++)
+    {
+        sum.fetch_add(-1);
+        //sum--;
+    }
 }
+
 int main()
 {
-    vector<std::thread> ThreadList;
-    for (int i = 0; i < 12; i++)
-    {
-        ThreadList.push_back(std::thread(HelloThread_2, i));
-    }
-    for (int i = 0; i < 12; i++)
-    {
-        if (ThreadList[i].joinable())
-            ThreadList[i].join();
-    }
-    /*
+    Add();
+    Sub();
 
-    std::thread t;
+    cout << sum << endl;
 
-    auto id1 = t.get_id(); // 쓰레드가 할당되지 않았으므로 0 을 return
+    std::thread t1(Add);
+    std::thread t2(Sub);
 
-    t = std::thread(HelloThread);
-    std::thread(HelloThread_2, 10);
-    
-    int32 count = t.hardware_concurrency();// CPU 코어 개수의 정보를 추출
+    t1.join();
+    t2.join();
 
-    auto id = t.get_id();  // 쓰레드 ID 를 return
-    //t.detach(); // std::thread 객체에서 실제 쓰레드를 분리 ( 리눅스의 Demon Process )
-
-    if (t.joinable()) // thread 변수 t 에 실제 스레드가 할당되어 있는지를 Check 
-        t.join();  // Main Thread 가 t Thread 가 끝날때까지 대기한다.
-        */
-
-    cout << "Hello main" << endl;
-
-     
-    
+    cout << sum << endl;
 }
 
